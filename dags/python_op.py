60c72402cede4ef4521f4b7afbded7147e3960c8
johar21 <joharamandeep99@gmail.com>
2025-02-08 10:06:56 +0530

    python dag added

--- dags/python_op.py
+++ dags/python_op.py
@@ -3,14 +3,18 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
 
-# Define a simple Python function
+# Define Python functions
 def print_hello():
-    return "Hello, Airflow!"
+    print("Hello, Airflow!")
+
+
+def print_bye():
+    print("Bye!!")
 
 
 # Default arguments for the DAG
 default_args = {
-    "owner": "airflow",
+    "owner": "test",
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
@@ -19,16 +23,21 @@ default_args = {
 with DAG(
     dag_id="python_operator_example",
     default_args=default_args,
-    start_date=datetime(2025, 5, 8),
+    start_date=datetime(2024, 2, 1),  # Set a past date
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
 
-    # Define the PythonOperator task
+    # Define PythonOperator tasks
     python_task = PythonOperator(
-        task_id="say_hello",
+        task_id="hello",
         python_callable=print_hello,  # Function to execute
     )
 
-    # Define task dependencies (if any)
-    python_task
+    python_bye = PythonOperator(
+        task_id="bye",
+        python_callable=print_bye,  # Function to execute
+    )
+
+    # Define task dependencies
+    python_task >> python_bye  # hello → bye
